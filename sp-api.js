4cc1c6c88a1a2b23f7d4c0afe81de9aa3b3e3563
vlad <senioreighteen@yandex.ru>
2020-12-30 17:22:04 +0500

    Merge request.js to main code

--- sp-api.js
+++ sp-api.js
@@ -1,4 +1,32 @@
-const request = require('./request');
+const
+	{ stringify } = require('querystring'), 
+    { request } = require('https');
+
+function send(params, postData) {
+	return new Promise((resolve, reject) => {
+		let req = request(params, (res) => {
+			res.setEncoding('utf8');
+			res.on('data', (data) => resolve( JSON.parse(data) ));
+		});
+
+		req.on('error', reject);
+
+		if(postData !== null) req.write(postData);
+		req.end();
+	});
+}
+
+function request(params) {
+	params.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+	let postData;
+    if(params.body) {
+		postData = stringify( params.body );
+		params.headers['Content-Length'] = Buffer.byteLength(postData);
+	}
+	
+	return send(params, (params.body) ? postData : null);
+};
+
 module.exports = function({ project_id, token, response_key }) {
     if(!project_id || !token || !response_key) throw "[sp-api] Не указаны нужные данные для работы с SP-API";
     
